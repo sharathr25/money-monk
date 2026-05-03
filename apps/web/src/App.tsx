6c71b8b365d59786187d5cfdea73626fa446cecf
Sharath <47311049+sharathr25@users.noreply.github.com>
2026-05-03 18:49:39 +0530

    add settings screen

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -4,6 +4,7 @@ import { CashFlow } from "@/pages/CashFlow"
 import { Goals } from "@/pages/Goals"
 import { AddGoal } from "@/pages/AddGoal"
 import { Header } from "./components/Header"
+import { Settings } from "./pages/settings"
 
 const Layout = () => (
   <div className="flex min-h-svh flex-1 flex-col">
@@ -25,6 +26,7 @@ export function App() {
           <Route path="cash-flow" element={<CashFlow />} />
           <Route path="goals" element={<Goals />} />
           <Route path="add-goal" element={<AddGoal />} />
+          <Route path="settings" element={<Settings />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ apps/web/src/pages/settings/Settings.tsx
@@ -0,0 +1,7 @@
+export function Settings() {
+  return (
+    <div className="flex">
+      <h1 className="font-medium">Settings</h1>
+    </div>
+  )
+}
