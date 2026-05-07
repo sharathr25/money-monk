efdbceb2add29f5cd469dd79d5ff510532396cd0
Sharath <47311049+sharathr25@users.noreply.github.com>
2026-05-07 22:09:04 +0530

    add cashflow movement

--- apps/web/src/pages/cashFlowManagment/CashFlowManagement.tsx
+++ apps/web/src/pages/cashFlowManagment/CashFlowManagement.tsx
@@ -1,11 +1,20 @@
+import { Badge } from "@workspace/ui/components/badge"
 import { Button } from "@workspace/ui/components/button"
+import { Card } from "@workspace/ui/components/card"
+import {
+  Item,
+  ItemContent,
+  ItemDescription,
+  ItemMedia,
+  ItemTitle,
+} from "@workspace/ui/components/item"
 import {
   Tabs,
   TabsContent,
   TabsList,
   TabsTrigger,
 } from "@workspace/ui/components/tabs"
-import { Plus } from "lucide-react"
+import { Banknote, Pen, Plus, Trash } from "lucide-react"
 import { useNavigate } from "react-router"
 
 export function CashFlowManagement() {
@@ -28,8 +37,138 @@ export function CashFlowManagement() {
             Expense
           </TabsTrigger>
         </TabsList>
-        <TabsContent value="income"></TabsContent>
-        <TabsContent value="expense"></TabsContent>
+        <TabsContent value="income" className="flex flex-col gap-3">
+          <Card className="p-0">
+            <Item>
+              <ItemMedia>
+                <Badge className="size-10" variant="secondary">
+                  <Banknote />
+                </Badge>
+              </ItemMedia>
+              <ItemContent>
+                <ItemTitle className="line-clamp-1">Salary</ItemTitle>
+                <ItemDescription>Monthly credit</ItemDescription>
+              </ItemContent>
+              <ItemContent className="flex flex-col items-end">
+                <ItemDescription>+ ₹1,500</ItemDescription>
+                <div className="flex gap-3">
+                  <Button
+                    variant="ghost"
+                    className="p-0 text-(--primary)"
+                    size="xs"
+                  >
+                    <Pen />
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    className="p-0 text-(--destructive)"
+                    size="xs"
+                  >
+                    <Trash />
+                  </Button>
+                </div>
+              </ItemContent>
+            </Item>
+          </Card>
+          <Card className="p-0">
+            <Item>
+              <ItemMedia>
+                <Badge className="size-10" variant="secondary">
+                  <Banknote />
+                </Badge>
+              </ItemMedia>
+              <ItemContent>
+                <ItemTitle className="line-clamp-1">Salary</ItemTitle>
+                <ItemDescription>Monthly credit</ItemDescription>
+              </ItemContent>
+              <ItemContent className="flex flex-col items-end">
+                <ItemDescription>+ ₹1,500</ItemDescription>
+                <div className="flex gap-3">
+                  <Button
+                    variant="ghost"
+                    className="p-0 text-(--primary)"
+                    size="xs"
+                  >
+                    <Pen />
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    className="p-0 text-(--destructive)"
+                    size="xs"
+                  >
+                    <Trash />
+                  </Button>
+                </div>
+              </ItemContent>
+            </Item>
+          </Card>
+        </TabsContent>
+        <TabsContent value="expense" className="flex flex-col gap-3">
+          <Card className="p-0">
+            <Item>
+              <ItemMedia>
+                <Badge className="size-10" variant="secondary">
+                  <Banknote />
+                </Badge>
+              </ItemMedia>
+              <ItemContent>
+                <ItemTitle className="line-clamp-1">Salary</ItemTitle>
+                <ItemDescription>Monthly credit</ItemDescription>
+              </ItemContent>
+              <ItemContent className="flex flex-col items-end">
+                <ItemDescription>- ₹1,500</ItemDescription>
+                <div className="flex gap-3">
+                  <Button
+                    variant="ghost"
+                    className="p-0 text-(--primary)"
+                    size="xs"
+                  >
+                    <Pen />
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    className="p-0 text-(--destructive)"
+                    size="xs"
+                  >
+                    <Trash />
+                  </Button>
+                </div>
+              </ItemContent>
+            </Item>
+          </Card>
+          <Card className="p-0">
+            <Item>
+              <ItemMedia>
+                <Badge className="size-10" variant="secondary">
+                  <Banknote />
+                </Badge>
+              </ItemMedia>
+              <ItemContent>
+                <ItemTitle className="line-clamp-1">Salary</ItemTitle>
+                <ItemDescription>Monthly credit</ItemDescription>
+              </ItemContent>
+              <ItemContent className="flex flex-col items-end">
+                <ItemDescription>- ₹1,500</ItemDescription>
+                <div className="flex gap-3">
+                  <Button
+                    variant="ghost"
+                    className="p-0 text-(--primary)"
+                    size="xs"
+                  >
+                    <Pen />
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    className="p-0 text-(--destructive)"
+                    size="xs"
+                  >
+                    <Trash />
+                  </Button>
+                </div>
+              </ItemContent>
+            </Item>
+          </Card>
+        </TabsContent>
       </Tabs>
       <Button
         className="fixed right-6 bottom-6 h-12 w-12 rounded-full"
