975a0480bdfae2cb8a2009be8ebf01715a941fe5
Sharath <47311049+sharathr25@users.noreply.github.com>
2026-05-08 19:34:28 +0530

    refactor add cash flow screen

--- apps/web/src/pages/addCashFlowMovement/AddCashFlowMovement.tsx
+++ apps/web/src/pages/addCashFlowMovement/AddCashFlowMovement.tsx
@@ -13,7 +13,6 @@ import {
   CardTitle,
 } from "@workspace/ui/components/card"
 import { Button } from "@workspace/ui/components/button"
-import { Label } from "@workspace/ui/components/label"
 import { Input } from "@workspace/ui/components/input"
 import {
   InputGroup,
@@ -23,7 +22,6 @@ import {
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -34,19 +32,14 @@ import {
 } from "@workspace/ui/components/radio-group"
 import {
   BanknoteArrowDown,
-  Calendar,
   MoveDown,
   MoveUp,
   Repeat,
   Save,
+  Calendar,
 } from "lucide-react"
 import * as Icons from "lucide-react"
-import {
-  Field,
-  FieldDescription,
-  FieldLabel,
-  FieldTitle,
-} from "@workspace/ui/components/field"
+import { Field, FieldLabel, FieldTitle } from "@workspace/ui/components/field"
 import { Calendar as CalendarInput } from "@workspace/ui/components/calendar"
 import { useState, type SubmitEventHandler } from "react"
 
@@ -65,7 +58,19 @@ const Form = ({
 
   const filteredIcons = ICON_NAMES.filter((name) =>
     name.toLowerCase().includes(icon.toLowerCase())
-  ).slice(0, 5)
+  )
+    .slice(0, 5)
+    .map((name) => {
+      const Icon = Icons[name] as Icons.LucideIcon
+
+      if (!Icon) return
+
+      return (
+        <div key={name} className="flex flex-1 flex-col items-center text-sm">
+          <Icon />
+        </div>
+      )
+    })
 
   return (
     <Card className="w-full">
@@ -76,67 +81,47 @@ const Form = ({
         <CardAction>{type === "INCOME" ? <MoveDown /> : <MoveUp />}</CardAction>
       </CardHeader>
       <CardContent>
-        <form className="flex flex-col gap-6" onSubmit={onSubmit}>
-          <div className="flex flex-col gap-2">
-            <Field>
-              <FieldLabel htmlFor="name">Name</FieldLabel>
-              <InputGroup className="h-11">
-                <InputGroupInput id="name" />
-                <InputGroupAddon align="inline-end">
-                  <Dialog>
-                    <DialogTrigger asChild>
-                      <BanknoteArrowDown />
-                    </DialogTrigger>
-                    <DialogContent>
-                      <DialogHeader>
-                        <DialogTitle>Icon</DialogTitle>
-                      </DialogHeader>
-                      <Input
-                        id="icon"
-                        onChange={(e) => setIcon(e.target.value)}
-                        value={icon}
-                        className="h-11"
-                        placeholder="Search icons..."
-                      />
-                      <div className="flex flex-wrap justify-start gap-4">
-                        {icon &&
-                          filteredIcons.map((name) => {
-                            const Icon = Icons[name] as Icons.LucideIcon
-
-                            if (!Icon) return
-
-                            return (
-                              <div
-                                key={name}
-                                className="flex flex-1 flex-col items-center text-sm"
-                              >
-                                <Icon />
-                              </div>
-                            )
-                          })}
-                      </div>
-                    </DialogContent>
-                  </Dialog>
-                </InputGroupAddon>
-              </InputGroup>
-            </Field>
-          </div>
-          <div className="flex flex-col gap-2">
-            <Label htmlFor="amount">Amount</Label>
+        <form className="flex flex-col gap-4" onSubmit={onSubmit}>
+          <Field>
+            <FieldLabel htmlFor="name">Name</FieldLabel>
+            <InputGroup className="h-11">
+              <InputGroupInput id="name" />
+              <InputGroupAddon align="inline-end">
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <BanknoteArrowDown />
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Icon</DialogTitle>
+                    </DialogHeader>
+                    <Input
+                      id="icon"
+                      onChange={(e) => setIcon(e.target.value)}
+                      value={icon}
+                      className="h-11"
+                      placeholder="Search icons..."
+                    />
+                    <div className="flex flex-wrap justify-start gap-4">
+                      {icon && filteredIcons}
+                    </div>
+                  </DialogContent>
+                </Dialog>
+              </InputGroupAddon>
+            </InputGroup>
+          </Field>
+          <Field>
+            <FieldLabel htmlFor="name">Amount</FieldLabel>
             <Input id="amount" type="number" required className="h-11" />
-          </div>
-          <div className="flex flex-col gap-2">
-            <Label>Frequency</Label>
+          </Field>
+          <Field>
+            <FieldLabel>Frequency</FieldLabel>
             <RadioGroup
               defaultValue={frequency}
               value={frequency}
               className="flex flex-1"
             >
-              <FieldLabel
-                htmlFor="monthly"
-                className="p-3"
-                onClick={() => setFrequency("MONTH")}
-              >
+              <FieldLabel htmlFor="monthly">
                 <Field
                   orientation="horizontal"
                   className="flex flex-1 justify-center"
@@ -146,53 +131,76 @@ const Form = ({
                   <RadioGroupItem
                     value="MONTH"
                     id="monthly"
-                    className="hidden"
+                    onClick={() => setFrequency("MONTH")}
                   />
                 </Field>
               </FieldLabel>
-              <FieldLabel
-                htmlFor="one-time"
-                className="p-3"
-                onClick={() => setFrequency("ONE_TIME")}
-              >
+              <FieldLabel htmlFor="one-time">
                 <Field orientation="horizontal">
                   <Calendar />
-                  <div>
-                    <FieldTitle>One time</FieldTitle>
-                    <FieldDescription className="flex gap-2">
-                      {date?.toLocaleDateString()}
-                    </FieldDescription>
-                  </div>
-                  <Dialog>
-                    <DialogTrigger asChild>
-                      <RadioGroupItem
-                        value="ONE_TIME"
-                        id="one-time"
-                        className="hidden"
-                      />
-                    </DialogTrigger>
-                    <DialogContent>
-                      <DialogHeader>
-                        <DialogTitle>Date</DialogTitle>
-                        <DialogDescription>
-                          If you don't select date, today's date will be taken
-                          by default. Click 'One time' frequency again to change
-                          date.
-                        </DialogDescription>
-                      </DialogHeader>
-                      <CalendarInput
-                        mode="single"
-                        selected={date}
-                        onSelect={setDate}
-                        captionLayout="dropdown"
-                        className="w-full rounded-lg border"
-                      />
-                    </DialogContent>
-                  </Dialog>
+                  <FieldTitle>One time</FieldTitle>
+                  <RadioGroupItem
+                    value="ONE_TIME"
+                    id="one-time"
+                    onClick={() => setFrequency("ONE_TIME")}
+                  />
                 </Field>
               </FieldLabel>
             </RadioGroup>
-          </div>
+          </Field>
+          <Field>
+            <FieldLabel htmlFor="date-picker-simple">Date</FieldLabel>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button
+                  disabled={frequency !== "ONE_TIME"}
+                  variant="outline"
+                  id="date-picker-simple"
+                  className="h-11 justify-start font-normal"
+                >
+                  <Calendar />
+                  {date?.toLocaleDateString()}
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-sm">
+                <DialogHeader>
+                  <DialogTitle>Date</DialogTitle>
+                </DialogHeader>
+                <CalendarInput
+                  required={frequency === "ONE_TIME"}
+                  mode="single"
+                  selected={date}
+                  onSelect={setDate}
+                  defaultMonth={date}
+                  captionLayout="dropdown"
+                  className="w-full"
+                />
+              </DialogContent>
+            </Dialog>
+            {/* <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  disabled={frequency !== "ONE_TIME"}
+                  variant="outline"
+                  id="date-picker-simple"
+                  className="h-11 justify-start font-normal"
+                >
+                  <Calendar />
+                  {date?.toLocaleDateString()}
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0" align="start">
+                <CalendarInput
+                  required={frequency === "ONE_TIME"}
+                  mode="single"
+                  selected={date}
+                  onSelect={setDate}
+                  defaultMonth={date}
+                  captionLayout="dropdown"
+                />
+              </PopoverContent>
+            </Popover> */}
+          </Field>
           <Button type="submit" className="h-13 w-full">
             <Save />
             Save
@@ -204,6 +212,11 @@ const Form = ({
 }
 
 export function AddCashFlowMovement() {
+  const onSubmit = (e: React.SubmitEvent) => {
+    e.preventDefault()
+    console.log("submitted")
+  }
+
   return (
     <div className="flex flex-1 flex-col gap-2">
       <div>
@@ -212,18 +225,18 @@ export function AddCashFlowMovement() {
       </div>
       <Tabs defaultValue="income">
         <TabsList className="flex w-full flex-1">
-          <TabsTrigger className="flex w-full flex-1 p-3" value="income">
+          <TabsTrigger className="flex w-full flex-1" value="income">
             Income
           </TabsTrigger>
-          <TabsTrigger className="flex w-full flex-1 p-3" value="expense">
+          <TabsTrigger className="flex w-full flex-1" value="expense">
             Expense
           </TabsTrigger>
         </TabsList>
         <TabsContent value="income">
-          <Form onSubmit={console.log} type="INCOME" />
+          <Form onSubmit={onSubmit} type="INCOME" />
         </TabsContent>
         <TabsContent value="expense">
-          <Form onSubmit={console.log} type="EXPENSE" />
+          <Form onSubmit={onSubmit} type="EXPENSE" />
         </TabsContent>
       </Tabs>
     </div>
