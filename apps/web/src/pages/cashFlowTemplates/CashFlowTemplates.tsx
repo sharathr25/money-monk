518e9ad65bfd381c3d02b9e982321c8a599c47ed
Sharath <47311049+sharathr25@users.noreply.github.com>
2026-05-12 09:51:33 +0530

    add loading and empty states in cash flow templates

--- apps/web/src/pages/cashFlowTemplates/CashFlowTemplates.tsx
+++ apps/web/src/pages/cashFlowTemplates/CashFlowTemplates.tsx
@@ -3,9 +3,16 @@ import { ROUTE_NAMES } from "@/routes"
 import { getLoggedInUser } from "@workspace/api/auth/index"
 import { DynamicIcon, type IconName } from "lucide-react/dynamic"
 import { queryCashFlowTemplates } from "@workspace/api/db/index"
-import type { CashFlowTemplate } from "@workspace/core/types"
-import { Badge } from "@workspace/ui/components/badge"
+import type { CashFlowTemplate, Type } from "@workspace/core/types"
 import { Button } from "@workspace/ui/components/button"
+import { Avatar, AvatarFallback } from "@workspace/ui/components/avatar"
+import {
+  Empty,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@workspace/ui/components/empty"
 import { Card } from "@workspace/ui/components/card"
 import {
   Item,
@@ -21,21 +28,107 @@ import {
   TabsTrigger,
 } from "@workspace/ui/components/tabs"
 import { formatAmount } from "@workspace/ui/lib/utils"
-import { Pen, Plus, Trash } from "lucide-react"
+import { Pen, Plus, Trash, FolderCode } from "lucide-react"
 import { useEffect, useState } from "react"
+import { Spinner } from "@workspace/ui/components/spinner"
 
 export function CashFlowTemplates() {
   const user = getLoggedInUser()
   const { navigate } = useNavigator()
   const [income, setIncome] = useState<CashFlowTemplate[]>([])
   const [expenses, setExpenses] = useState<CashFlowTemplate[]>([])
+  const [loading, setLoading] = useState(false)
+
+  const init = async () => {
+    try {
+      setLoading(true)
+      const templates = await queryCashFlowTemplates({ uid: user.uid || "" })
+      setIncome(templates.filter((t) => t.type === "INCOME"))
+      setExpenses(templates.filter((t) => t.type === "EXPENSE"))
+    } catch (error) {
+    } finally {
+      setLoading(false)
+    }
+  }
 
   useEffect(() => {
-    queryCashFlowTemplates({ uid: user.uid || "" }).then((cfts) => {
-      setIncome(cfts.filter((t) => t.type === "INCOME"))
-      setExpenses(cfts.filter((t) => t.type === "EXPENSE"))
-    })
+    init()
   }, [])
+
+  const Templates = ({
+    templates,
+    type,
+  }: {
+    templates: CashFlowTemplate[]
+    type: Type
+  }) => {
+    if (loading) return <Spinner className="m-auto" />
+
+    if (templates.length === 0)
+      return (
+        <Empty className="border border-dashed">
+          <EmptyHeader>
+            <EmptyMedia variant="icon">
+              <FolderCode />
+            </EmptyMedia>
+            <EmptyTitle className="capitalize">
+              No {type.toLowerCase()} Yet
+            </EmptyTitle>
+            <EmptyDescription>
+              Start by adding your first {type.toLowerCase()} below
+            </EmptyDescription>
+          </EmptyHeader>
+        </Empty>
+      )
+
+    return (
+      <>
+        {income.map((i) => (
+          <Card className="p-0" key={i.id}>
+            <Item className="flex items-center">
+              <ItemMedia>
+                <Avatar size="lg" className="after:border-0">
+                  <AvatarFallback className="rounded-none">
+                    <DynamicIcon name={i.icon as IconName} />
+                  </AvatarFallback>
+                </Avatar>
+              </ItemMedia>
+              <ItemContent>
+                <ItemTitle className="line-clamp-1">{i.name}</ItemTitle>
+                <ItemDescription>{i.description}</ItemDescription>
+              </ItemContent>
+              <ItemContent className="flex flex-col items-end">
+                <ItemDescription>
+                  {type === "INCOME" ? "+" : "-"} {formatAmount(`${i.amount}`)}
+                </ItemDescription>
+                <div className="flex gap-3">
+                  <Button
+                    variant="ghost"
+                    className="p-0 text-(--primary)"
+                    size="xs"
+                    onClick={() =>
+                      navigate(ROUTE_NAMES.EDIT_CASH_FLOW_TEMPLATE, {
+                        templateId: i.id,
+                      })
+                    }
+                  >
+                    <Pen />
+                  </Button>
+                  <Button
+                    variant="ghost"
+                    className="p-0 text-(--destructive)"
+                    size="xs"
+                  >
+                    <Trash />
+                  </Button>
+                </div>
+              </ItemContent>
+            </Item>
+          </Card>
+        ))}
+      </>
+    )
+  }
 
   return (
     <div className="flex flex-1 flex-col gap-2">
@@ -55,90 +148,10 @@ export function CashFlowTemplates() {
           </TabsTrigger>
         </TabsList>
         <TabsContent value="income" className="flex flex-col gap-3">
-          {income.map((i) => (
-            <Card className="p-0" key={i.id}>
-              <Item>
-                <ItemMedia>
-                  <Badge className="size-10" variant="secondary">
-                    <DynamicIcon name={i.icon as IconName} />
-                  </Badge>
-                </ItemMedia>
-                <ItemContent>
-                  <ItemTitle className="line-clamp-1">{i.name}</ItemTitle>
-                  <ItemDescription>{i.description}</ItemDescription>
-                </ItemContent>
-                <ItemContent className="flex flex-col items-end">
-                  <ItemDescription>
-                    + {formatAmount(`${i.amount}`)}
-                  </ItemDescription>
-                  <div className="flex gap-3">
-                    <Button
-                      variant="ghost"
-                      className="p-0 text-(--primary)"
-                      size="xs"
-                      onClick={() =>
-                        navigate(ROUTE_NAMES.EDIT_CASH_FLOW_TEMPLATE, {
-                          templateId: i.id,
-                        })
-                      }
-                    >
-                      <Pen />
-                    </Button>
-                    <Button
-                      variant="ghost"
-                      className="p-0 text-(--destructive)"
-                      size="xs"
-                    >
-                      <Trash />
-                    </Button>
-                  </div>
-                </ItemContent>
-              </Item>
-            </Card>
-          ))}
+          <Templates templates={income} type="INCOME" />
         </TabsContent>
         <TabsContent value="expense" className="flex flex-col gap-3">
-          {expenses.map((e) => (
-            <Card className="p-0" key={e.id}>
-              <Item>
-                <ItemMedia>
-                  <Badge className="size-10" variant="secondary">
-                    <DynamicIcon name={e.icon as IconName} />
-                  </Badge>
-                </ItemMedia>
-                <ItemContent>
-                  <ItemTitle className="line-clamp-1">{e.name}</ItemTitle>
-                  <ItemDescription>{e.description}</ItemDescription>
-                </ItemContent>
-                <ItemContent className="flex flex-col items-end">
-                  <ItemDescription>
-                    - {formatAmount(`${e.amount}`)}
-                  </ItemDescription>
-                  <div className="flex gap-3">
-                    <Button
-                      variant="ghost"
-                      className="p-0 text-(--primary)"
-                      size="xs"
-                      onClick={() =>
-                        navigate(ROUTE_NAMES.EDIT_CASH_FLOW_TEMPLATE, {
-                          templateId: e.id,
-                        })
-                      }
-                    >
-                      <Pen />
-                    </Button>
-                    <Button
-                      variant="ghost"
-                      className="p-0 text-(--destructive)"
-                      size="xs"
-                    >
-                      <Trash />
-                    </Button>
-                  </div>
-                </ItemContent>
-              </Item>
-            </Card>
-          ))}
+          <Templates templates={expenses} type="EXPENSE" />
         </TabsContent>
       </Tabs>
       <Button
--- packages/ui/src/lib/utils.ts
+++ packages/ui/src/lib/utils.ts
@@ -1,7 +1,11 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-const AMOUNT_FORMATER = new Intl.NumberFormat("en-IN", { currency: "INR" })
+const AMOUNT_FORMATER = new Intl.NumberFormat("en-IN", {
+  currency: "INR",
+  style: "currency",
+})
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
