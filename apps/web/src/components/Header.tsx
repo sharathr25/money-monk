e857718513747a491214888125b0b0e8c23a6366
Sharath <47311049+sharathr25@users.noreply.github.com>
2026-05-15 09:07:38 +0530

    remove route name from header

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,17 +1,9 @@
 import { useNavigator } from "@/hooks/useNavigator"
 import { Button } from "@workspace/ui/components/button"
 import { MoveLeft } from "lucide-react"
-import { useLocation } from "react-router"
 
 export function Header() {
   const { goBack } = useNavigator()
-  const location = useLocation()
-  const { pathname } = location
-
-  const paths = pathname.split("/")
-  const currentRoute = paths[paths.length - 1]
-  const routeName =
-    currentRoute === "" ? "cash flow" : currentRoute.replaceAll("-", " ")
 
   return (
     <div className="my-2 flex items-center gap-2">
@@ -20,7 +12,6 @@ export function Header() {
           <MoveLeft />
         </Button>
       )}
-      <div className="capitalize">{routeName.toLowerCase()}</div>
     </div>
   )
 }
