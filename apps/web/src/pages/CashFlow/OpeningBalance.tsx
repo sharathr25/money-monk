36941db1b87a8d7d69b1a2bb02d5d63994c4bffa
Sharath <47311049+sharathr25@users.noreply.github.com>
2026-06-05 06:18:55 +0530

    change edit button placement in opening balance card

--- apps/web/src/pages/CashFlow/OpeningBalance.tsx
+++ apps/web/src/pages/CashFlow/OpeningBalance.tsx
@@ -6,7 +6,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@workspace/ui/components/card"
-import { Pen, Save, X } from "lucide-react"
+import { Pen, Save, Wallet, X } from "lucide-react"
 
 import { amountToDouble, formatAmount } from "@workspace/ui/lib/utils"
 import { Input } from "@workspace/ui/components/input"
@@ -46,48 +46,47 @@ export function OpeningBalance({
           Opening Balance
         </CardTitle>
         <CardAction>
-          <AlertDialog>
-            <AlertDialogTrigger asChild>
-              <Button variant="link" className="p-0">
-                <Pen className="size-5" />
-              </Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent size="sm">
-              <AlertDialogHeader className="flex flex-col items-start">
-                <AlertDialogTitle>Change Opening Balance</AlertDialogTitle>
-              </AlertDialogHeader>
-              <AlertDialogDescription></AlertDialogDescription>
-              <Field className="basis-3/4">
-                <FieldLabel htmlFor="opening-balance">
-                  Opening Balance
-                </FieldLabel>
-                <Input
-                  id="opening-balance"
-                  className="h-11"
-                  value={balance}
-                  onChange={(e) => setBalance(formatAmount(e.target.value))}
-                />
-              </Field>
-              <AlertDialogFooter className="border-0">
-                <AlertDialogCancel>
-                  Cancel
-                  <X />
-                </AlertDialogCancel>
-
-                <AlertDialogAction
-                  disabled={openingBalance === amountToDouble(balance)}
-                  onClick={() => updateOpeningBalance(amountToDouble(balance))}
-                >
-                  Save
-                  <Save />
-                </AlertDialogAction>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
+          <Wallet />
         </CardAction>
       </CardHeader>
-      <CardContent className="mt-auto text-xl">
+      <CardContent className="mt-auto flex items-center justify-between text-xl">
         {formatAmount(openingBalance, { withCurrency: true })}
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button variant="link" className="p-0" size="sm">
+              <Pen className="size-5" />
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent size="sm">
+            <AlertDialogHeader className="flex flex-col items-start">
+              <AlertDialogTitle>Change Opening Balance</AlertDialogTitle>
+            </AlertDialogHeader>
+            <AlertDialogDescription></AlertDialogDescription>
+            <Field className="basis-3/4">
+              <FieldLabel htmlFor="opening-balance">Opening Balance</FieldLabel>
+              <Input
+                id="opening-balance"
+                className="h-11"
+                value={balance}
+                onChange={(e) => setBalance(formatAmount(e.target.value))}
+              />
+            </Field>
+            <AlertDialogFooter className="border-0">
+              <AlertDialogCancel>
+                Cancel
+                <X />
+              </AlertDialogCancel>
+
+              <AlertDialogAction
+                disabled={openingBalance === amountToDouble(balance)}
+                onClick={() => updateOpeningBalance(amountToDouble(balance))}
+              >
+                Save
+                <Save />
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </CardContent>
     </Card>
   )
